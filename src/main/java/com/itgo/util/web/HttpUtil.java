5281ea368506ec069eaacdbda8bcde7b8fbd4fdd
xigexb <xigexb@outlook.com>
2019-06-23 22:40:12 +0800

    url编码

--- src/main/java/com/itgo/util/web/HttpUtil.java
+++ src/main/java/com/itgo/util/web/HttpUtil.java
@@ -13,10 +13,7 @@ import javax.net.ssl.TrustManager;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -80,7 +77,9 @@ public class HttpUtil {
                     params = "?" + str.substring(0, str.length() - 1);
                 }
             }
-            URL url = new URL(urlAddr + params);
+            String urlPath = Utf8URLencode(urlAddr + params);
+            URL url = new URL(urlPath);
+            logger.info("Request URL {}",urlPath);
             conn = (HttpURLConnection) url.openConnection();
             // 设置读取超时时间
             conn.setReadTimeout(readTimeout);
@@ -266,6 +265,7 @@ public class HttpUtil {
                 }
             }
             URL url = new URL(urlAddr + params);
+            logger.info("Request URL {}",urlAddr + params);
             conn = (HttpsURLConnection) url.openConnection();
             // 设置读取超时时间
             conn.setReadTimeout(readTimeout);
@@ -747,5 +747,108 @@ public class HttpUtil {
             }
         }
         return result.toString();
+    }
+
+    /**
+     * Utf8URL编码
+     * @param s
+     * @return
+     */
+    public static  String Utf8URLencode(String text) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c >= 0 && c <= 255) {
+                result.append(c);
+            }else {
+                byte[] b = new byte[0];
+                try {
+                    b = Character.toString(c).getBytes("UTF-8");
+                }catch (Exception ex) {
+                }
+                for (int j = 0; j < b.length; j++) {
+                    int k = b[j];
+                    if (k < 0) k += 256;
+                    result.append("%" + Integer.toHexString(k).toUpperCase());
+                }
+            }
+        }
+        return result.toString();
+    }
+    /**
+     * Utf8URL解码
+     * @param text
+     * @return
+     */
+    public static  String Utf8URLdecode(String text) {
+        String result = "";
+        int p = 0;
+        if (text!=null && text.length()>0){
+            text = text.toLowerCase();
+            p = text.indexOf("%e");
+            if (p == -1) return text;
+            while (p != -1) {
+                result += text.substring(0, p);
+                text = text.substring(p, text.length());
+                if (text == "" || text.length() < 9) return result;
+
+                result += CodeToWord(text.substring(0, 9));
+                text = text.substring(9, text.length());
+                p = text.indexOf("%e");
+            }
+        }
+        return result + text;
+    }
+    /**
+     * utf8URL编码转字符
+     * @param text
+     * @return
+     */
+    private static String CodeToWord(String text) {
+        String result;
+        if (Utf8codeCheck(text)) {
+            byte[] code = new byte[3];
+            code[0] = (byte) (Integer.parseInt(text.substring(1, 3), 16) - 256);
+            code[1] = (byte) (Integer.parseInt(text.substring(4, 6), 16) - 256);
+            code[2] = (byte) (Integer.parseInt(text.substring(7, 9), 16) - 256);
+            try {
+                result = new String(code, "UTF-8");
+            }catch (UnsupportedEncodingException ex) {
+                result = null;
+            }
+        }
+        else {
+            result = text;
+        }
+        return result;
+    }
+    /**
+     * 编码是否有效
+     * @param text
+     * @return
+     */
+    private static  boolean Utf8codeCheck(String text){
+        String sign = "";
+        if (text.startsWith("%e"))
+            for (int i = 0, p = 0; p != -1; i++) {
+                p = text.indexOf("%", p);
+                if (p != -1)
+                    p++;
+                sign += p;
+            }
+        return sign.equals("147-1");
+    }
+    /**
+     * 判断是否Utf8Url编码
+     * @param text
+     * @return
+     */
+    public static  boolean isUtf8Url(String text) {
+        text = text.toLowerCase();
+        int p = text.indexOf("%");
+        if (p != -1 && text.length() - p > 9) {
+            text = text.substring(p, p + 9);
+        }
+        return Utf8codeCheck(text);
     }
 }
